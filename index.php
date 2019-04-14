dca73acf2139a9b5c868da569fc57a04dc6b5cd4
Josh Dow <joshua.dow@ucalgary.ca>
2019-04-14 15:07:39 -0600

    Fixed???

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
 <?php
-require_once "recaptchalib.php";
+  $email;$comment;$captcha;
+  $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+  $comment = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_STRING);
+  $captcha = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
+  if(!$captcha){
+    echo '<h2>Please check the the captcha form.</h2>';
+    exit;
+  }
+  $secretKey = "6LcDKZ4UAAAAAIgD3x_3nY83rJclRgVzfDjSMzpo";
+  $ip = $_SERVER['REMOTE_ADDR'];
 
-$secret = "6LcDKZ4UAAAAAIgD3x_3nY83rJclRgVzfDjSMzpo";
+  // post request to server
+  $url = 'https://www.google.com/recaptcha/api/siteverify';
+  $data = array('secret' => $secretKey, 'response' => $captcha);
 
-$response = null;
-
-$reCaptcha = new ReCaptcha($secret);
-
-if ($_POST["g-recaptcha-response"]) {
-    $response = $reCaptcha->verifyResponse(
-        $_SERVER["REMOTE_ADDR"],
-        $_POST["g-recaptcha-response"]
-    );
-}
+  $options = array(
+    'http' => array(
+      'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+      'method'  => 'POST',
+      'content' => http_build_query($data)
+    )
+  );
+  $context  = stream_context_create($options);
+  $response = file_get_contents($url, false, $context);
+  $responseKeys = json_decode($response,true);
+  header('Content-type: application/json');
+  if($responseKeys["success"]) {
+    echo json_encode(array('success' => 'true'));
+  } else {
+    echo json_encode(array('success' => 'false'));
+  }
 ?>

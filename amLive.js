08defa74e55237093ae82188a6b1c056a669d819
Josh Dow <joshua.dow@ucalgary.ca>
2019-04-19 16:51:04 -0600

    Added pdfs to new system and played around with new menu system.

--- amLive.js
+++ amLive.js
@@ -2,5 +2,8 @@ function ifIAmLive(){
 	console.log("hello")
 	if(document.getElementById("logo").src.match("imgs/twitchON.PNG")){
 	  document.getElementById("MS").style.visibility="visible";
+	  document.getElementById("MS2").style.visibility="visible";
+  	  document.getElementById("MS").style.display="block";
+	  document.getElementById("MS2").style.display="block";
 	}
 }
--- showVis.js
+++ showVis.js
@@ -2,6 +2,7 @@ function showVis(id){
 
 	var one = id.getAttribute('id');
 	document.getElementById('editor').innerHTML = '';
+	document.getElementById('pdfContainer').style.display = "none";
 	document.getElementById('editor').className = codeStore[one].type + " hljs"
 
 	document.getElementById('editor').innerHTML = codeStore[one] && codeStore[one].code ? codeStore[one].code : 'This section is Empty! '
@@ -9,3 +10,28 @@ function showVis(id){
 	document.querySelectorAll('pre code').forEach((block) => {
 	  hljs.highlightBlock(block);
 })};
+
+function showPDF(id){
+	var one = id.getAttribute('id');
+	document.getElementById('editor').innerHTML = '';
+	document.getElementById('pdfEmbed').src = '';
+	document.getElementById('totalContainer').style.display = "none";
+	switch(one){
+		case "pdf1":
+			document.getElementById('pdfEmbed').src = "imgs/418Assigment1(1).pdf";
+			break;
+		case "pdf2":
+			document.getElementById('pdfEmbed').src = "imgs/418Assignment2(1).pdf";
+			break;
+		case "pdf3":
+			document.getElementById('pdfEmbed').src = "imgs/418Assignment3(1).pdf";
+			break;
+		case "pdf4":
+			document.getElementById('pdfEmbed').src = "imgs/PMAT_315_Assignment_1.pdf";
+			break;
+		case "pdf5":
+			document.getElementById('pdfEmbed').src = "imgs/PMAT_315_Assignment_2.pdf";
+			break;
+	}
+	document.getElementById('pdfContainer').style.display = "block";
+};

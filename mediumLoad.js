acbadadc2399e54644ca6d49847eb2257e4f230f
Josh Dow <joshua.dow@ucalgary.ca>
2019-04-21 21:31:39 -0600

    fixed rss??

--- mediumLoad.js
+++ mediumLoad.js
@@ -4,7 +4,7 @@ $(function () {
             var data = {
                 rss: 'https://medium.com/feed/@Comparabl'
             };
-            $.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40Comparabl&api_key=svkqpl9gta415ln5ytqqzmgbvgynlxix3pmr1duf', data, function (response) {
+            $.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40Comparabl', data, function (response) {
                 if (response.status == 'ok') {
                     $("#logo").append(`<img src="${response.feed["image"]}" class="rounded mx-auto d-block">`)
                     var output = '';
